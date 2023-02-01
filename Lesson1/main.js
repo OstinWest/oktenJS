612ac13b8252f74a6cf8783720ea63418bf4b98c
OstinWest <olexii.bilokon@gmail.com>
2023-02-01 17:20:41 +0200

    LS1

---
+++ Lesson1/main.js
@@ -0,0 +1,45 @@
+let w1 = "hello";
+console.log(w1);
+let w2 = "owu";
+console.log(w2);
+let w3 = "com";
+console.log(w3);
+let w4 = "ua";
+console.log(w4);
+
+let n1 = 1;
+console.log(n1);
+let n2 = 10;
+console.log(n2);
+let n3 = -999;
+console.log(n3);
+let n4 = 123;
+console.log(n4);
+let n5 = 16;
+console.log(n5);
+
+let nf1 = 3.14;
+console.log(nf1);
+let nf2 = 2.17;
+console.log(nf2);
+
+let bl1 = 10;
+let bl2 = 16;
+console.log(bl1<bl2);
+console.log(bl1>bl2);
+
+let firstName = "Oleksii ";
+let middleName = "Oleksiiovych ";
+let lastName = "Bilokon ";
+let person = firstName + middleName + lastName;
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log (typeof a);
+console.log (typeof b);
+console.log (typeof c);
+
+
+
